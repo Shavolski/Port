2ffe3b9baaa4206ebd70715396a3913f154a5dbc
Shavolski-dev <stevewachiradev@gmail.com>
2020-12-26 18:30:30 -0500

    greetings and home components created

--- app/src/App.js
+++ app/src/App.js
@@ -1,11 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
+
+
+import Greetings from './components/greetings';
+import Home from './components/home';
 
 function App() {
   return (
     <main>
         <Switch>
-
+          <Route path="/" component={Greetings} exact />
+          <Route path="/Home" component={Home} />
         </Switch>
     </main>
   );
